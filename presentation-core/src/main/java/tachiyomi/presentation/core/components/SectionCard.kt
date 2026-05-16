55243055d88ba44833b7988c9b29a4b7ff73367e
thiskiro <whothatshiro@gmail.com>
2026-05-16 17:57:52 +0700

    fix: sort imports in SectionCard.kt

--- presentation-core/src/main/java/tachiyomi/presentation/core/components/SectionCard.kt
+++ presentation-core/src/main/java/tachiyomi/presentation/core/components/SectionCard.kt
@@ -5,8 +5,8 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
